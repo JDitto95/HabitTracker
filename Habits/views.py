3f126c8979faeaef2b8eb3c27b51f5df2d2d2d4f
JDitto95 <powell0295@gmail.com>
2022-05-19 19:12:44 -0400

    worked through an error caused in views.py/edit_habit

--- Habits/views.py
+++ Habits/views.py
@@ -8,6 +8,8 @@ from .forms import HabitForm
 def list_habits(request):
     habits = Habit.objects.all()
     return render(request, "habits/list_habits.html", {"habits": habits})
+
+
 @login_required
 def add_habits(request):
     if request.method == 'GET':
@@ -21,6 +23,8 @@ def add_habits(request):
             return redirect(to='list_habits')
             
     return render( request, "habits/add_habit.html", {"form": form})
+
+
 @login_required
 def edit_habit(request, pk):
     habit = get_object_or_404(Habit, pk=pk)
@@ -30,7 +34,9 @@ def edit_habit(request, pk):
         form = HabitForm(data=request.POST, instance=habit)
         if form.is_valid():
             form.save()
-            return redirect(to='list_habits')
+        return redirect(to='list_habits')
+
+    return render( request, "habits/edit_habit.html", {"form": form, "habit": habit})
 
 @login_required
 def delete_habit(request, pk):
@@ -40,21 +46,7 @@ def delete_habit(request, pk):
         return redirect(to='list_habits')
     return render(request, "habits/delete_habit.html", {"habit": habit}) 
 
-@login_required
-def edit_habit(request, pk):
-    habit = get_object_or_404(Habit, pk=pk)
-    if request.method == 'GET':
-        form = HabitForm(instance=habit)
-    else:
-        form = HabitForm(data=request.POST, instance=habit)
-        if form.is_valid():
-            form.save()
-            return redirect(to='list_habits')
 
-    return render(request, "habits/edit_habit.html", {
-        "form": form,
-        "habit": habit
-    })     
     
 def habit_detail(request, pk):
     habit = get_object_or_404(Habit, pk=pk)
